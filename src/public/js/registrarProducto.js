d65c44cee4ed9b33c89511b00c4df1d8aea54583
Nosferatu721 <elkintorres721@gmail.com>
2022-08-23 00:24:37 -0500

    Produccion Terminada

--- src/public/js/registrarProducto.js
+++ src/public/js/registrarProducto.js
@@ -1,11 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const tablaProduccionBody = document.querySelector("#tablaProduccion tbody"),
-    TotalRegistros = document.getElementById("TotalRegistros");
+document.addEventListener('DOMContentLoaded', () => {
+  const tablaProduccionBody = document.querySelector('#tablaProduccion tbody'),
+    TotalRegistros = document.getElementById('TotalRegistros');
 
-  getData("/registrosProductosHoy").then((res) => {
-    console.log(res);
+  getData('/registrosProductosHoy').then((res) => {
     let objProductos = {},
-      htmlTR = "",
+      htmlTR = '',
       suma = 0;
     res.forEach((producto) => {
       if (objProductos[`${producto.PRO_Referencia}`]) {
@@ -30,5 +29,60 @@ document.addEventListener("DOMContentLoaded", () => {
     TotalRegistros.innerHTML = suma;
 
     tablaProduccionBody.innerHTML = htmlTR;
+  });
+
+  // * Filtro
+  const containerTableFilter = document.getElementById('containerTableFilter'),
+    btnFiltro = document.getElementById('btnFiltro'),
+    fechaInput = document.getElementById('fechaInput');
+
+  btnFiltro.addEventListener('click', async () => {
+    let res = await postData('/getForDate', { fecha: fechaInput.value });
+    if (res.length === 0) {
+      Toast.fire({
+        icon: 'info',
+        title: 'No hay registros en esta fecha 😢',
+      });
+      return
+    }
+
+    let objProductos = {},
+      htmlTR = `
+        <table class='striped highlight centered'>
+          <thead>
+            <tr>
+              <th>Referencia</th>
+              <th>Cantidad</th>
+            </tr>
+          </thead>
+
+          <tbody>
+          `,
+      suma = 0;
+    res.forEach((producto) => {
+      if (objProductos[`${producto.PRO_Referencia}`]) {
+        let suma = (objProductos[`${producto.PRO_Referencia}`] += producto.PRO_Cantidad);
+        objProductos[`${producto.PRO_Referencia}`] = suma;
+      } else {
+        objProductos[`${producto.PRO_Referencia}`] = producto.PRO_Cantidad;
+      }
+    });
+
+    let claves = Object.keys(objProductos);
+    for (let i = 0; i < claves.length; i++) {
+      suma += objProductos[claves[i]];
+      htmlTR += `
+          <tr>
+            <td>${claves[i]}</td>
+            <td>${objProductos[claves[i]]}</td>
+          </tr>
+        `;
+    }
+    htmlTR += `</tbody>
+    </table>`;
+
+    htmlTR += `<h4 class='center'>Total <b>${suma}</b></h4>`;
+
+    containerTableFilter.innerHTML = htmlTR;
   });
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,18 @@
-const router = require("express").Router();
-const db = require("../database");
+const router = require('express').Router();
+const db = require('../database');
 
 // ? Usuarios
-router.get("/", (req, res) => {
-  res.redirect("/login");
+router.get('/', (req, res) => {
+  res.redirect('/login');
 });
 
-router.get("/listaUsuarios", async (req, res) => {
+router.get('/listaUsuarios', async (req, res) => {
   const sqlSelect = `SELECT * FROM users`;
   let [resultSelect] = await db.promise().query(sqlSelect);
-  res.render("factoria/listaUsuarios", { title: "Lista Usuario", resultSelect });
+  res.render('factoria/listaUsuarios', { title: 'Lista Usuario', resultSelect });
 });
 
-router.post("/actualizar/:id", async (req, res) => {
+router.post('/actualizar/:id', async (req, res) => {
   const ID = req.params.id,
     data = {
       USR_PrimerNombre: req.body.nombre,
@@ -22,35 +22,56 @@ router.post("/actualizar/:id", async (req, res) => {
   const sqlUpdate = `UPDATE users SET ? WHERE USR_ID = ${ID}`;
   let [resultUpdate] = await db.promise().query(sqlUpdate, [data]);
   if (resultUpdate.affectedRows) {
-    res.redirect("/listaUsuarios");
+    res.redirect('/listaUsuarios');
   } else {
-    res.json({ error: "Error 500" });
+    res.json({ error: 'Error 500' });
   }
 });
 
 // ? Produccion
-router.get("/resgistrarProducto", async (req, res) => {
-  res.render("factoria/registrarProducto", { title: "Productos" });
+router.get('/resgistrarProducto', async (req, res) => {
+  res.render('factoria/registrarProducto', { title: 'Productos' });
 });
 
-router.post("/resgistrarProducto", async (req, res) => {
+router.post('/resgistrarProductoEmpanadas', async (req, res) => {
   const data = {
     PRO_Referencia: req.body.referenciaEmpanadas,
-    PRO_Cantidad: req.body.cantidad,
+    PRO_Cantidad: req.body.cantidadEmpanada,
+    PRO_Tipo: 'Empanadas',
   };
   const sqlInsert = `INSERT INTO produccion SET ?`;
   let [resultInsert] = await db.promise().query(sqlInsert, [data]);
-  res.redirect('/resgistrarProducto')
+  req.flash('messageSuccess', `Se agrego <b>${req.body.cantidadEmpanada}</b> de <b>${req.body.referenciaEmpanadas}</b> 😁`);
+  res.redirect('/resgistrarProducto');
+});
+
+router.post('/resgistrarProductoPizza', async (req, res) => {
+  const data = {
+    PRO_Referencia: req.body.referenciaPizza,
+    PRO_Cantidad: req.body.cantidadPizza,
+    PRO_Tipo: 'Pizza',
+  };
+  const sqlInsert = `INSERT INTO produccion SET ?`;
+  let [resultInsert] = await db.promise().query(sqlInsert, [data]);
+  req.flash('messageSuccess', `Se agrego <b>${req.body.cantidadPizza}</b> de <b>${req.body.referenciaPizza}</b> 😁`);
+  res.redirect('/resgistrarProducto');
 });
 
 router.get('/registrosProductosHoy', async (req, res) => {
-  let FechaHoy = new Date()
-  let FormatFechaHoy = FechaHoy.toISOString().split('T')[0]
+  let FechaHoy = new Date();
+  let FormatFechaHoy = FechaHoy.toISOString().split('T')[0];
   console.log(FormatFechaHoy);
 
-  const sqlSelect = `SELECT * FROM produccion WHERE DATE(PRO_Created_at) = ?`
-  let [resultSelect] = await db.promise().query(sqlSelect, [FormatFechaHoy])
-  res.json(resultSelect)
-})
+  const sqlSelect = `SELECT * FROM produccion WHERE DATE(PRO_Created_at) = ?`;
+  let [resultSelect] = await db.promise().query(sqlSelect, [FormatFechaHoy]);
+  res.json(resultSelect);
+});
+
+router.post('/getForDate', async (req, res) => {
+  const { fecha } = req.body;
+  const sqlSelect = `SELECT * FROM produccion WHERE DATE(PRO_Created_at) = ?`;
+  let [resultSelect] = await db.promise().query(sqlSelect, [fecha]);
+  res.json(resultSelect);
+});
 
 module.exports = router;
